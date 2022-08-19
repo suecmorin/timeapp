b680a69351f4dc61b85c0258f4f9925bf34d07ab
suecmorin <suecmorin@gmail.com>
2022-08-19 04:13:10 -0700

    debugged iteration loop

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Work Day Scheduler Starter Code
+
+NAME Work Day Scheduler 
+
+PROJECT STATUS Project completed August 2022.
+
+DESCRIPTION Project uses Javascript with HTML and CSS to generate a day planner. 
+
+MOTIVATION To demonstrate understanding of Javascript functions and code; also to try using "this" as javascript is object-oriented.
+
+CODE STYLE standard
+
+FEATURES Allows user to see past, present and upcoming appointments and enter new ones.
+
+INSTALLATION Files were uploaded to GitHub.
+
+SUPPORT Please contact Sue Morin, suecmorin@gmail.com.
+
+IMAGE Screenshot (8).png
+
+CONTRIBUTING 
--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 var currentDayEl = $("#currentDay");
-
-//var now = moment().hours();      
-
-function displayTime() {
+var saveBtnEl = $("#saveBtn");
+var now = moment().hours();   
   //display the time at the top of the page
+  function displayTime() {
   var rightNow = moment().format("dddd MM / DD [at] hh:mm a");
 
-  currentDayEl.text(rightNow);
-}
-    
+  currentDayEl.text(rightNow);   
+  }
 
 setInterval(displayTime, 1000); //updates the time at the top of the page every second
 
 showTimeappt();
 
-function showTimeappt() {
+function showTimeappt(){
     var time = $(".time-block");
-    time.each(function() {                                //each replaces for loop, runs on each .time-block in HTML/parse takes each id separately/
-    var hour = parseInt($(this).attr("id"));                     //Int converts string "9" to integer to do the math comparisons    
-                                                                       //this refers to .time-block
+    time.each(function(){                                //each replaces for loop, runs on each .time-block in HTML/parse takes each id separately/
+    var hour = parseInt($(this).attr("id"));            //Int converts string "9" to integer to do the math comparisons                                                                       //this refers to .time-block
     if (now > hour) {                                                //compare hour with id in class "time-block" in HTML
-      $(this).children("col-sm-10").attr("class", "past col-sm-10 description");      //if current hour is greater than id, add class ".past" from style.css file
-                                                                                     //textarea is a child of description class
+      $(this).children(".col-sm-10").attr("class", "past col-sm-10 description");      //if current hour is greater than id, add class ".past" from style.css file
+                                                                                            //textarea is a child of description class
     } else if (now === hour) {
-      $(this).children("col-sm-10").attr("class", "present col-sm-10 description");
+      $(this).children(".col-sm-10").attr("class", "present col-sm-10 description");
 
     } else {
-      $(this).children("col-sm-10").attr("class", "future col-sm-10 description");
+      $(this).children(".col-sm-10").attr("class", "future col-sm-10 description");
     }
+    console.log(time.children);   
   })
 }
 
-button.on("click", (function(event) {
+saveBtnEl.onclick = function(){event()};
+
+function event() {
  event.preventDefault();
- var descrip = document.GetElementById("mtg").value;
+var descrip = document.GetElementById("mtg").value;
 var parent = $(this).parent().attr("id");                                    //this refers to textarea "mtg", parent is time-block
 localStorage.setItem(parent, descrip);
+localStorage.getItem(parent,descrip);
+}
 
-});
