e56e5103dddd1c918f80c741083ea82c45a32ebc
suecmorin <suecmorin@gmail.com>
2022-11-14 12:15:42 -0700

    Local Storage working correctly

--- script.js
+++ script.js
@@ -13,13 +13,9 @@ $(document).ready(function () {
     var appt= $(this).siblings(".description").val(); 
     var hour = $(this).parent().attr("id");                            
     localStorage.setItem(hour, appt); 
-    console.log(localStorage.getItem("12"));
-    console.log(localStorage.getItem("13"));
   });
- // retrieveLocal();
 });
 
-//retrieveLocal();
 showTimeappt();  
 retrieveLocal();
 
@@ -43,13 +39,13 @@ function showTimeappt(){
 };
 
 function retrieveLocal() {
-  $("#9.description").val(localStorage.getItem("9"));
-  $("#10.description").val(localStorage.getItem("10"));
-  $("#11.description").val(localStorage.getItem("11"));
-  $("#12.description").val(localStorage.getItem("12"));
-  $("#13.description").val(localStorage.getItem("13"));
-  $("#14.description").val(localStorage.getItem("14"));
-  $("#15.description").val(localStorage.getItem("15"));
-  $("#16.description").val(localStorage.getItem("16"));
-  $("#17.description").val(localStorage.getItem("17"));
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
   };
