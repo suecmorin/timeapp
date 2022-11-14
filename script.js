7f748343f6be4e1b27a8e4285f36362726e17df9
suecmorin <suecmorin@gmail.com>
2022-11-14 11:58:21 -0700

    setItem working

--- script.js
+++ script.js
@@ -1,42 +1,33 @@
 var currentDayEl = $("#currentDay");
-var saveBtnEl = $("#saveBtn");
+var saveBtnEl = $(".saveBtn");
 var now = moment().hours();   
 
+displayTime();
+setInterval(displayTime, 1000);
+
 $(document).ready(function () {
-  saveBtnEl.onclick = function() {
+  saveBtnEl.on('click', function() {
     function e() {
      e.preventDefault();
     };
     var appt= $(this).siblings(".description").val(); 
     var hour = $(this).parent().attr("id");                            
     localStorage.setItem(hour, appt); 
-    console.log(localStorage.getItem("11"));
-  };
+    console.log(localStorage.getItem("12"));
+    console.log(localStorage.getItem("13"));
+  });
+ // retrieveLocal();
 });
 
-displayTime();
-setInterval(displayTime, 1000);
-showTimeappt();
+//retrieveLocal();
+showTimeappt();  
 retrieveLocal();
-  
 
 function displayTime() {
   var rightNow = moment().format("dddd MM / DD [at] hh:mm a");
   currentDayEl.text(rightNow);   
-  }
- 
-  function retrieveLocal() {
-  $("#9.description").val(localStorage.getItem("9"));
-  $("#10.description").val(localStorage.getItem("10"));
-  $("#11.description").val(localStorage.getItem("11"));
-  $("#12.description").val(localStorage.getItem("12"));
-  $("#13.description").val(localStorage.getItem("13"));
-  $("#14.description").val(localStorage.getItem("14"));
-  $("#15.description").val(localStorage.getItem("15"));
-  $("#16.description").val(localStorage.getItem("16"));
-  $("#17.description").val(localStorage.getItem("17"));
   };
-
+ 
 function showTimeappt(){
     var time = $(".time-block");
     time.each(function(){                                
@@ -47,8 +38,18 @@ function showTimeappt(){
       $(this).children(".col-sm-10").attr("class", "present col-sm-10 description");
       } else {
       $(this).children(".col-sm-10").attr("class", "future col-sm-10 description");
-      }  
+      }; 
     });
 };
 
-
+function retrieveLocal() {
+  $("#9.description").val(localStorage.getItem("9"));
+  $("#10.description").val(localStorage.getItem("10"));
+  $("#11.description").val(localStorage.getItem("11"));
+  $("#12.description").val(localStorage.getItem("12"));
+  $("#13.description").val(localStorage.getItem("13"));
+  $("#14.description").val(localStorage.getItem("14"));
+  $("#15.description").val(localStorage.getItem("15"));
+  $("#16.description").val(localStorage.getItem("16"));
+  $("#17.description").val(localStorage.getItem("17"));
+  };
