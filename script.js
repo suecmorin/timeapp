4aa6e35348ec69e250be5ab28d6342d89a0e86fc
suecmorin <suecmorin@gmail.com>
2022-11-14 10:24:31 -0700

    working on local storage

--- script.js
+++ script.js
@@ -2,8 +2,30 @@ var currentDayEl = $("#currentDay");
 var saveBtnEl = $("#saveBtn");
 var now = moment().hours();   
 
-$(document).ready(function(){
-  console.log(localStorage);
+$(document).ready(function () {
+  saveBtnEl.onclick = function() {
+    function e() {
+     e.preventDefault();
+    };
+    var appt= $(this).siblings(".description").val(); 
+    var hour = $(this).parent().attr("id");                            
+    localStorage.setItem(hour, appt); 
+    console.log(localStorage.getItem("11"));
+  };
+});
+
+displayTime();
+setInterval(displayTime, 1000);
+showTimeappt();
+retrieveLocal();
+  
+
+function displayTime() {
+  var rightNow = moment().format("dddd MM / DD [at] hh:mm a");
+  currentDayEl.text(rightNow);   
+  }
+ 
+  function retrieveLocal() {
   $("#9.description").val(localStorage.getItem("9"));
   $("#10.description").val(localStorage.getItem("10"));
   $("#11.description").val(localStorage.getItem("11"));
@@ -13,42 +35,20 @@ $(document).ready(function(){
   $("#15.description").val(localStorage.getItem("15"));
   $("#16.description").val(localStorage.getItem("16"));
   $("#17.description").val(localStorage.getItem("17"));
-});
-
-  function displayTime() {
-  var rightNow = moment().format("dddd MM / DD [at] hh:mm a");
-  currentDayEl.text(rightNow);   
-  }
-
-setInterval(displayTime, 1000); //updates the time at the top of the page every second
-
-showTimeappt();
+  };
 
 function showTimeappt(){
     var time = $(".time-block");
     time.each(function(){                                
     var hour = parseInt($(this).attr("id"));            //Int converts string "9" to integer to do the math comparisons                                                                       //this refers to .time-block
     if (now > hour) {                                                
-      $(this).children(".col-sm-10").attr("class", "past col-sm-10 description");      
-                                                                                            
-    } else if (now === hour) {
+      $(this).children(".col-sm-10").attr("class", "past col-sm-10 description");                                                                                   
+      } else if (now === hour) {
       $(this).children(".col-sm-10").attr("class", "present col-sm-10 description");
-
-    } else {
+      } else {
       $(this).children(".col-sm-10").attr("class", "future col-sm-10 description");
-    }
-    console.log(time.children);   
-  });
+      }  
+    });
 };
 
-saveBtnEl.onclick = function(){
-
-function e() {
- e.preventDefault();
-};
-var parent = $(this).parent().attr("id"); 
-var descrip = $(this).child(".col-sm-10").value();                                  
-localStorage.setItem(parent, descrip);
-console.log(localStorage);
-};
 
